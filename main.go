3fa8476ddf191343ba9e689988d2f692da014686
Konstantine Babuadze <konstantine.babuadze@dreamapply.com>
2019-06-27 21:32:05 +0300

    Trying to change WEBUI

--- main.go
+++ main.go
@@ -8,9 +8,9 @@ import (
 
 	"github.com/gorilla/pat"
 	"github.com/ian-kent/go-log/log"
+	"github.com/kbabuadze/MailHog-UI/assets"
 	"github.com/kbabuadze/MailHog-UI/config"
-	"github.com/mailhog/MailHog-UI/assets"
-	"github.com/mailhog/MailHog-UI/web"
+	"github.com/kbabuadze/MailHog-UI/web"
 	comcfg "github.com/mailhog/MailHog/config"
 	"github.com/mailhog/http"
 )
