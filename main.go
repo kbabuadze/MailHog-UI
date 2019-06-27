ccc627d4b841c7600795b8c1f14ad5a2d4c361da
Konstantine Babuadze <konstantine.babuadze@dreamapply.com>
2019-06-27 18:05:58 +0300

    Uiconf

--- main.go
+++ main.go
@@ -8,8 +8,8 @@ import (
 
 	"github.com/gorilla/pat"
 	"github.com/ian-kent/go-log/log"
+	"github.com/kbabuadze/MailHog-UI/config"
 	"github.com/mailhog/MailHog-UI/assets"
-	"github.com/mailhog/MailHog-UI/config"
 	"github.com/mailhog/MailHog-UI/web"
 	comcfg "github.com/mailhog/MailHog/config"
 	"github.com/mailhog/http"
